9736000b611460a15a79f8717a1b9184aa104daa
shabesa <k.a.shabesa@gmail.com>
2025-11-08 07:34:50 +0530

    fixed import and added trace file outputs

--- interrupts_101258619_101298449.cpp
+++ interrupts_101258619_101298449.cpp
@@ -5,7 +5,7 @@
  *
  */
 
-#include "interrupts.hpp"
+#include "interrupts_101258619_101298449.hpp"
 
 std::tuple<std::string, std::string, int> simulate_trace(std::vector<std::string> trace_file, int time, std::vector<std::string> vectors, std::vector<int> delays, std::vector<external_file> external_files, PCB current, std::vector<PCB> wait_queue) {
 
